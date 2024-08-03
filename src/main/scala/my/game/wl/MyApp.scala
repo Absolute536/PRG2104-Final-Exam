4e4b439183e0488132866af8b6ca86f8bdeb907a
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-03 17:05:34 +0800

    Create package structure

---
+++ src/main/scala/my/game/wl/MyApp.scala
@@ -0,0 +1,13 @@
+package my.game.wl
+
+import scalafx.application.JFXApp
+/*
+import javafx.{scene => jfxs}
+import scalafx.Includes._
+import scalafx.application.JFXApp._
+import scalafx.scene.Scene
+import scalafxml.core.{FXMLLoader, NoDependencyResolver}
+*/
+object MyApp extends JFXApp {
+
+}
