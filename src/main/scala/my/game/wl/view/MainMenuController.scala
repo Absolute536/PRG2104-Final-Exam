0dd1d8518f0814970b5f8ef17c3442366f073047
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-06 00:43:42 +0800

    Adjusted Title margin of MainMenu and modified the controller accordingly

--- src/main/scala/my/game/wl/view/MainMenuController.scala
+++ src/main/scala/my/game/wl/view/MainMenuController.scala
@@ -7,11 +7,30 @@ import my.game.wl.MainApp
 
 @sfxml
 class MainMenuController {
+  def startGame(): Unit = {
+
+  }
+
+  def selectDifficulty(): Unit = {
+
+  }
+
   def showHelp():Unit = {
     val resource = getClass.getResource("Help.fxml")
     val loader = new FXMLLoader(resource, NoDependencyResolver)
     loader.load
     val roots = loader.getRoot[jfxs.layout.BorderPane]
-    MainApp.main.setCenter(roots)
+    MainApp.roots.setCenter(roots)
+  }
+  def showScoreBoard(): Unit = {
+
+  }
+
+  def showSettings(): Unit = {
+
+  }
+
+  def showCredit(): Unit = {
+
   }
 }
