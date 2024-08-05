ee54af3fb0d5a43f87b307a19667035595019fca
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-05 20:59:56 +0800

    Migrate button action to MainMenuController for better structure

--- src/main/scala/my/game/wl/view/MainMenuController.scala
+++ src/main/scala/my/game/wl/view/MainMenuController.scala
@@ -1,11 +1,17 @@
 package my.game.wl.view
 
 import scalafxml.core.macros.sfxml
+import javafx.{scene => jfxs}
+import scalafxml.core.{FXMLLoader, NoDependencyResolver}
 import my.game.wl.MainApp
 
 @sfxml
 class MainMenuController {
   def showHelp():Unit = {
-    MainApp.showHelp()
+    val resource = getClass.getResource("Help.fxml")
+    val loader = new FXMLLoader(resource, NoDependencyResolver)
+    loader.load
+    val roots = loader.getRoot[jfxs.layout.BorderPane]
+    MainApp.main.setCenter(roots)
   }
 }
