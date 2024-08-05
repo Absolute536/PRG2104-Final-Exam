bef0b1cf6a72a39d5f551b6cecb0222f6a05983f
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-05 17:56:32 +0800

    Added Main Menu booting in MainApp

--- src/main/scala/my/game/wl/MainApp.scala
+++ src/main/scala/my/game/wl/MainApp.scala
@@ -1,13 +1,24 @@
 package my.game.wl
 
 import scalafx.application.JFXApp
-/*
 import javafx.{scene => jfxs}
 import scalafx.Includes._
 import scalafx.application.JFXApp._
 import scalafx.scene.Scene
 import scalafxml.core.{FXMLLoader, NoDependencyResolver}
-*/
+
 object MainApp extends JFXApp {
+  val mainMenu = getClass.getResource("view/MainMenu.fxml")
+  val loader = new FXMLLoader(mainMenu, NoDependencyResolver)
+  loader.load()
+  val main = loader.getRoot[jfxs.layout.AnchorPane]
+
+  stage = new PrimaryStage {
+    title = "Typer Defense"
+    scene = new Scene {
+      root = main
+    }
+  }
+
 
 }
