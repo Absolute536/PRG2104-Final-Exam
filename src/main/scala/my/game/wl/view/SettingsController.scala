2b6d142b9b9b3594bc5a475409aa2e08fad85b3a
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-13 00:20:03 +0800

    Testing settings toggle buttons

--- src/main/scala/my/game/wl/view/SettingsController.scala
+++ src/main/scala/my/game/wl/view/SettingsController.scala
@@ -1,14 +1,26 @@
 package my.game.wl.view
 
+import javafx.collections.ObservableList
+import scalafx.Includes._
 import scalafxml.core.macros.sfxml
 import my.game.wl.MainApp
 import scalafx.beans.property.StringProperty
-import scalafx.scene.control.Label
+import scalafx.scene.control.{Label, Toggle, ToggleGroup}
+import my.game.wl.util.Sound
+import my.game.wl.MainApp
+
 
 @sfxml
-class SettingsController {
+class SettingsController (
+                         private val backgroundMusic: ToggleGroup
+                         ) {
 
   def exitSettings(): Unit = {
     MainApp.showMainMenu()
+  }
+
+  def toggleBackgroundMusic(): Unit = {
+    // CHANGE THIS
+    backgroundMusic.selectedToggle.onChange(MainApp.s.stopBackgroundMusic())
   }
 }
