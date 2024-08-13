6dd36a174acc55be3a41495fed836e3da6dd6344
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-14 00:49:04 +0800

    Initial complete implementation of Sound

--- src/main/scala/my/game/wl/util/Sound.scala
+++ src/main/scala/my/game/wl/util/Sound.scala
@@ -2,16 +2,39 @@ package my.game.wl.util
 
 import scalafx.scene.media.{Media, MediaPlayer}
 
-import java.net.URI
-
 class Sound {
+  val backgroundMusic: MediaPlayer = new MediaPlayer(new Media(getClass.getResource("../../../../media/test.mp3").toString))
+  val soundEffect: MediaPlayer = new MediaPlayer(new Media(getClass.getResource("../../../../media/test2.mp3").toString))
+  var typingSound: MediaPlayer = new MediaPlayer(new Media(getClass.getResource("../../../../media/test2.mp3").toString))
+
 
   def playBackgroundMusic(): Unit = {
-    val mediaU: String = getClass.getResource("../../../../media/test.mp3").toString
-    val media: Media = new Media(mediaU)
-    val backgroundMusic = new MediaPlayer(media)
     backgroundMusic.autoPlay = true
     backgroundMusic.cycleCount = MediaPlayer.Indefinite
+  }
+
+  def stopBackgroundMusic(): Unit = {
+    backgroundMusic.stop()
+  }
+
+  def playSoundEffect(): Unit = {
+    soundEffect.play()
+  }
+
+  def disableSoundEffect(): Unit = {
+    soundEffect.volume = 0.0
+  }
+
+  def enableSoundEffect(): Unit = {
+    soundEffect.volume = 1.0
+  }
+
+  def setTypingSound(sound: Media): Unit = {
+    typingSound = new MediaPlayer(sound)
+  }
+
+  def playTypingSound(): Unit = {
+    typingSound.play()
   }
 
 }
