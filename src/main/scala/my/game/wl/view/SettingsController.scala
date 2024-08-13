db1c0074256564db4b9b3b60dd05d50648ddb623
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-14 01:30:13 +0800

    Trial implementation of settings

--- src/main/scala/my/game/wl/view/SettingsController.scala
+++ src/main/scala/my/game/wl/view/SettingsController.scala
@@ -1,18 +1,20 @@
 package my.game.wl.view
 
-import javafx.collections.ObservableList
 import scalafx.Includes._
+import javafx.{scene => jfxs}
 import scalafxml.core.macros.sfxml
 import my.game.wl.MainApp
 import scalafx.beans.property.StringProperty
-import scalafx.scene.control.{Label, Toggle, ToggleGroup}
+import scalafx.scene.control.{Label, RadioButton, Toggle, ToggleGroup}
 import my.game.wl.util.Sound
 import my.game.wl.MainApp
 
 
 @sfxml
 class SettingsController (
-                         private val backgroundMusic: ToggleGroup
+                         private val backgroundMusic: ToggleGroup,
+                         private val soundEffect: ToggleGroup,
+                         private val keyboardSound: ToggleGroup
                          ) {
 
   def exitSettings(): Unit = {
@@ -20,7 +22,34 @@ class SettingsController (
   }
 
   def toggleBackgroundMusic(): Unit = {
-    // CHANGE THIS
-    backgroundMusic.selectedToggle.onChange(MainApp.s.stopBackgroundMusic())
+    println(backgroundMusic.getSelectedToggle.asInstanceOf[jfxs.control.RadioButton].text.value)
+    if (backgroundMusic.getSelectedToggle.asInstanceOf[jfxs.control.RadioButton].text.value == "On") {
+      MainApp.s.playBackgroundMusic()
+    }
+    else {
+      MainApp.s.stopBackgroundMusic()
+    }
   }
+
+  def toggleSoundEffect(): Unit = {
+    if (soundEffect.getSelectedToggle.asInstanceOf[jfxs.control.RadioButton].text.value == "On") {
+      MainApp.s.playSoundEffect()
+    }
+    else {
+      MainApp.s.disableSoundEffect()
+    }
+  }
+
+  def toggleKeyboardSound(): Unit = {
+    if (keyboardSound.getSelectedToggle.asInstanceOf[jfxs.control.RadioButton].text.value == "Typing Sound 1") {
+      MainApp.s.setTypingSound(0)
+    }
+    else if (keyboardSound.getSelectedToggle.asInstanceOf[jfxs.control.RadioButton].text.value == "Typing Sound 2") {
+      MainApp.s.setTypingSound(1)
+    }
+    else {
+      MainApp.s.setTypingSound(2)
+    }
+  }
+
 }
