9a21ea66945cb3a6bac6d189825385716ed021be
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-14 19:02:17 +0800

    Implemented player score reading from text file & Scoreboard display in a TableView

--- src/main/scala/my/game/wl/view/ScoreBoardController.scala
+++ src/main/scala/my/game/wl/view/ScoreBoardController.scala
@@ -4,10 +4,33 @@ import scalafxml.core.macros.sfxml
 import javafx.{scene => jfxs}
 import scalafxml.core.{FXMLLoader, NoDependencyResolver}
 import my.game.wl.MainApp
+import scalafx.scene.control.{TableColumn, TableView}
+import my.game.wl.model.Score
+import scala.util.{Try, Success, Failure}
+
+import java.util.Comparator
 
 @sfxml
-class ScoreBoardController {
+class ScoreBoardController (
+                           private val scoreBoard:TableView[Score],
+                           private val playerNameColumn: TableColumn[Score, String],
+                           private val scorePointsColumn: TableColumn[Score, Int],
+                           private val timeSurvivedColumn: TableColumn[Score, String]
+                           ) {
+
+  if (MainApp.scores.isEmpty) {
+    println("The file is empty")
+  }
+  else {
+    scoreBoard.items = MainApp.scores
+    playerNameColumn.cellValueFactory = {_.value.playerName}
+    scorePointsColumn.cellValueFactory = {_.value.points}
+    timeSurvivedColumn.cellValueFactory = {_.value.timeSurvived}
+
+  }
+
   def handleExitScoreBoard(): Unit = {
     MainApp.showMainMenu()
   }
+
 }
