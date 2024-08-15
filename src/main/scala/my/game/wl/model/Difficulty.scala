9b33cc2a1b93a04149103467147121d8ecb986c5
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-15 15:43:09 +0800

    Difficulty selection scene further refinement

--- src/main/scala/my/game/wl/model/Difficulty.scala
+++ src/main/scala/my/game/wl/model/Difficulty.scala
@@ -1,5 +1,9 @@
 package my.game.wl.model
 
-class Difficulty (val state: String, val multiplier: Double) {
+case class Difficulty (state: String, multiplier: Double)
 
+case object Difficulty {
+  val easy = Difficulty("Easy", 0.85)
+  val medium = Difficulty("Medium", 1.00)
+  val hard = Difficulty("Hard", 1.25)
 }
--- src/main/scala/my/game/wl/view/DifficultyController.scala
+++ src/main/scala/my/game/wl/view/DifficultyController.scala
@@ -1,8 +1,11 @@
 package my.game.wl.view
 
+import scalafx.Includes._
 import scalafxml.core.macros.sfxml
 import my.game.wl.MainApp
 import scalafx.scene.control.ToggleGroup
+import javafx.{scene => jfxs}
+import my.game.wl.model.Difficulty
 
 @sfxml
 class DifficultyController (
@@ -14,6 +17,14 @@ class DifficultyController (
   }
 
   def startGame(): Unit = {
+    val selectedDifficulty = difficultyChoice.getSelectedToggle.asInstanceOf[jfxs.control.RadioButton]
 
+    selectedDifficulty.text.value match {
+      case "Easy" => MainApp.game.difficulty = Difficulty.easy
+      case "Medium" => MainApp.game.difficulty = Difficulty.medium
+      case "Hard" => MainApp.game.difficulty = Difficulty.hard
+    }
+
+    MainApp.showGame()
   }
 }
