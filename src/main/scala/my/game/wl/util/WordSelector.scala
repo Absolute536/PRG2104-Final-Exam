34f108bd5dec8907a63adce515386b9269621cfc
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-15 17:30:06 +0800

    Utility class WordSelector added

---
+++ src/main/scala/my/game/wl/util/WordSelector.scala
@@ -0,0 +1,27 @@
+package my.game.wl.util
+
+import my.game.wl.model.Difficulty
+
+import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
+import scala.util.{Failure, Random, Success, Try}
+
+class WordSelector {
+  private val wordList: ArrayBuffer[String] = ArrayBuffer[String]()
+
+  def initialiseWordList(difficulty: Difficulty): Unit = {
+    val wordListFile = Try({Source.fromFile(getClass.getResource(s"../../../../${difficulty.state}.txt").toURI)})
+    wordListFile match {
+      case Success(x) => for (word <- x.getLines()) {
+        wordList += word
+      }
+      x.close()
+      case Failure(x) => x.printStackTrace()
+    }
+  }
+
+  def generateWord(): String = {
+    wordList(Random.nextInt(wordList.length))
+  }
+
+}
