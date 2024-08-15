4901ed13460f690be5ac77782114bfde992bcc6d
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-16 02:53:15 +0800

    Added Pause alert dialog in game

--- src/main/scala/my/game/wl/model/Game.scala
+++ src/main/scala/my/game/wl/model/Game.scala
@@ -1,49 +1,16 @@
 package my.game.wl.model
 
-import my.game.wl.{MainApp, model}
-import scalafx.beans.property.{ObjectProperty, StringProperty}
 import scalafx.collections.ObservableBuffer
-
-import java.util.{Timer, TimerTask}
+import my.game.wl.MainApp
 
 class Game (
            val player: Player,
            val enemies: ObservableBuffer[Enemy],
-           val score: Score,
-           var difficulty: Difficulty
+           var difficulty: Difficulty,
+           val score: Score
            ) {
-  /*
-  def this(difficulty: Int) = {
-    this(new Player, new ObservableBuffer[Enemy](), new Score(player.name, player.point),
-      difficulty match {
-        case 1 => Difficulty.easy
-        case 2 => Difficulty.medium
-        case 3 => Difficulty.hard
-      })
+
+  def generateEnemy(): Unit = {
+    MainApp.game.enemies += Enemy.slime
   }
-
-   */
-
-  def launchGame(): Unit = {
-    val startTime = System.currentTimeMillis()
-
-    val timer = new Timer()
-    val tTask = new TimerTask {
-      override def run(): Unit = {
-        enemies.prepend(Enemy.spawn(startTime))
-      }
-    }
-    timer.schedule(tTask, 1000, 5000) // start spawn after 1 second, and spawn every 5 second
-
-
-    println("Game started")
-    while (player.healthPoint > 0) {
-
-
-    }
-    tTask.cancel()
-    println("Game End")
-
-  }
-
 }
--- src/main/scala/my/game/wl/view/GameController.scala
+++ src/main/scala/my/game/wl/view/GameController.scala
@@ -4,12 +4,15 @@ import scalafx.Includes._
 import scalafxml.core.macros.sfxml
 import my.game.wl.MainApp
 import javafx.{scene => jfxs}
-import scalafx.scene.control.Label
+import my.game.wl.model.Enemy
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.control.{Alert, ButtonType, Label}
 import scalafx.scene.image.{Image, ImageView}
-import scalafx.scene.input.{KeyCode, KeyEvent}
+import scalafx.scene.input.{KeyCode, KeyEvent, MouseEvent}
 import scalafx.scene.layout.{BorderPane, Pane}
 import scalafx.scene.shape.Line
 
+import java.util.{Timer, TimerTask}
 import scala.collection.mutable.ListBuffer
 
 @sfxml
@@ -32,7 +35,31 @@ class GameController (
 
   playerSprite.image = new Image(getClass.getResource("../../../../images/New Piskel(1).gif").toString)
 
+  def testBtn(): Unit = {
+    MainApp.game.enemies += Enemy.slime
+  }
 
+  def stageFocus(): Unit = {
+    stage.requestFocus()
+  }
+  def showPauseDialog(keyEvent: KeyEvent): Unit = {
+    println("Pressed")
+
+    if (keyEvent.code == KeyCode.Escape) {
+      val pauseAlert = new Alert(AlertType.Confirmation) {
+        title = "Pause Menu"
+        headerText = "Paused"
+        contentText = "The Game is Paused. Do you want to quit?"
+
+      }.showAndWait()
+
+      pauseAlert.get match {
+        case ButtonType.OK => MainApp.showMainMenu()
+        case _ => println("Resume")
+
+      }
+    }
+  }
 
 
 
