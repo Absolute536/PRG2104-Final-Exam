f30505cbf6e2b5851eccde127dd6498384fcdd0a
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-18 01:43:43 +0800

    Modified game name

--- src/main/scala/my/game/wl/MainApp.scala
+++ src/main/scala/my/game/wl/MainApp.scala
@@ -2,14 +2,17 @@ package my.game.wl
 
 import scalafx.application.JFXApp
 import javafx.{scene => jfxs}
-import my.game.wl.model.Score
+import my.game.wl.model.{Game, Score}
 import scalafx.Includes._
 import scalafx.application.JFXApp._
 import scalafx.scene.Scene
 import scalafxml.core.{FXMLLoader, NoDependencyResolver}
 import my.game.wl.util.Sound
 import scalafx.collections.ObservableBuffer
+import my.game.wl.model._
 
+
+import java.util.Timer
 import scala.io.Source
 import scala.util.{Failure, Success, Try}
 
@@ -21,7 +24,7 @@ object MainApp extends JFXApp {
   val roots = loader.getRoot[jfxs.layout.BorderPane]
 
   stage = new PrimaryStage {
-    title = "Typer Slime"
+    title = "Typer Defense"
     scene = new Scene {
       root = roots
     }
@@ -40,16 +43,36 @@ object MainApp extends JFXApp {
   val s: Sound = new Sound()
   s.playBackgroundMusic()
 
-  val scores = new ObservableBuffer[Score]()
+  val scoreBoard = new ObservableBuffer[Score]()
   // Maybe we should move it to some other place
   val scoreBoardFile = Try({Source.fromFile(getClass.getResource("../../../TopScore.txt").toURI)})
   scoreBoardFile match {
     case Success(x) => for (line <- x.getLines().map(l => l.split(","))) {
-      scores += new Score(line(0), line(1).toInt, line(2))
+      scoreBoard += new Score(line(0), line(1).toInt, line(2))
     }
       x.close()
     case Failure(e) => e.printStackTrace()
   }
+
+  val game = new Game(new Player)
+
+  var timer = new Timer(true)
+
+  def showGame(): Unit = {
+    val gameView = getClass.getResource("view/Game.fxml")
+    val loader = new FXMLLoader(gameView, NoDependencyResolver)
+    loader.load()
+    val gameStage = loader.getRoot[jfxs.layout.BorderPane]
+    this.roots.setCenter(gameStage)
+
+  }
+
+
+
+
+
+
+
 
 
 }
