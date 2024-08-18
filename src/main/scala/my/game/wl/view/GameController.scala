5bd4c08c15b048ad4740c902e3df49b226339e11
Wei Le Koh <weile.koh1286@gmail.com>
2024-08-19 01:44:45 +0800

    Further adjusted the movement and knockback of the enemy upon entering the correct characters

--- src/main/scala/my/game/wl/view/GameController.scala
+++ src/main/scala/my/game/wl/view/GameController.scala
@@ -78,7 +78,7 @@ class GameController (
       refreshWord()
       currentCharacter = 0
       MainApp.game.player.increasePoints(MainApp.game.difficulty.value)
-      enemySprite.translateX.value += 40
+      enemySprite.translateX.value += 40 * (gameStage.width.value / 850) // 850 is the initial width of the gameStage
       score.text = s"Score: ${currentScore.value.toString}"
       MainApp.game.sound.playSoundEffect()
     }
